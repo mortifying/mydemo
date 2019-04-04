c2b84cbd11838d72ebf476d0655369ac66d0bd29
yusb <yusb@mail.jointsky.com>
2019-04-04 15:00:04 +0800

    fastjson

--- src/main/java/com/mydemotest/CXFWSTest.java
+++ src/main/java/com/mydemotest/CXFWSTest.java
@@ -1,8 +1,11 @@
 package com.mydemotest;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+
+import java.util.List;
 
 /**
  * mydemo
@@ -16,10 +19,15 @@ public class CXFWSTest {
         String url = "http://10.61.176.219:800/PsMonitorWebService.asmx";
         String key = "NM_3E21VX97T";
         String procName = "srv.Proc_GetGasFacTenMinsData_ServiceUtil";
-        String paraJson = "{\"startDate\":\"2019-04-03 15:00:00\",\"endDate\":\"2019-04-03 17:00:00\",\"psCode\":\"610800000047\"}";
+        String paraJson = "{\"startDate\":\"2019-04-04 13:00:00\",\"endDate\":\"2019-04-04 14:00:00\",\"psCode\":\"610800000047\"}";
         //String[] paras = new String[]{url,key,procName,paraJson};
         Object[] result = client.invoke("GetPsMonitor",url,key,procName,paraJson);
-        System.out.println(result[0]);
-        System.out.println(JSON.parseArray(result[0].toString()));
+        //System.out.println(result[0]);
+        List<Object> subList = JSON.parseArray(result[0].toString()).subList(0,1);
+        //System.out.println(subList);
+        JSONObject json = (JSONObject)subList.get(0);
+        json.put("111", "2222222222");
+        System.out.println(json);
+
     }
 }
